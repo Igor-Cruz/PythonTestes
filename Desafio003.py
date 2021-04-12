1a5e6e6d50dc3c3bd2e45a03c740b2862e78e54d
Igor-Cruz <igorcruz.it@outlook.com>
2021-04-12 13:36:11 -0300

    Desafio 03 e 04, soma de valores e imprimindo o resultado e informações do tipo primitivo

---
+++ Desafio003.py
@@ -0,0 +1,5 @@
+# Criar um programa que leia dois n° e mostre a soma
+n1 = int(input('Digite um n°: '))
+n2 = int(input('Digite um n°: '))
+resultado = n1 + n2
+print(f'o resultado da soma do valores digitados em  n1: {n1} e n2: {n2} foi {resultado}')
---
+++ Desafio004.py
@@ -0,0 +1,5 @@
+# Informar o tipo primitivo
+a = input('Digite algo: ')
+print('O que você digitou é:',type(a))
+
+
